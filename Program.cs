0392d282666487f992d0f046c6b375a0bb4cadb2
Ethan Biggs <ethanscott402@gmail.com>
2025-01-22 09:02:15 -0700

    First program expansion

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
-﻿using FoodBank;
+﻿//Expand scope of program
+using FoodBank;
 
+//Initialize program
+Console.WriteLine("Program Loading...");
+try
+{
+    Interface ui = new Interface();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error Loading Program: " + e.Message);
+}
+
+//Print the menu
 Console.WriteLine("Welcome to the C# FoodBank!");
 Console.WriteLine("1. Add food item");
 Console.WriteLine("2. Delete food item");
@@ -7,13 +20,4 @@ Console.WriteLine("3. Print food items");
 Console.WriteLine("4. Exit");
 Console.Write("Enter your choice (1-4): ");
 
-while (true) //Don't let the user enter a non-number
-{
-    try {
-        int choice = int.Parse(Console.ReadLine());
-        break;
-    }
-    catch { 
-        Console.WriteLine("Invalid input. Please enter a number between 1 and 4.");
-    }
-}
+
