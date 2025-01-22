1ffa768713766d2db757b467c47cffbe1cc00103
Ethan Biggs <ethanscott402@gmail.com>
2025-01-22 16:59:20 -0700

    Final adjustments

--- BankTools.cs
+++ BankTools.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,105 @@ namespace FoodBank
 {
     internal class BankTools
     {
+        public BankTools() { }
 
+        public static List<FoodItem> AddFoodItem(List<FoodItem> bankMemory, string name, string category, int quantity, DateTime expirationDate)
+        {
+            bankMemory.Add(new FoodItem(name, category, quantity, expirationDate));
+            return bankMemory;
+        }
+
+        public static List<FoodItem> DeleteFoodItem(List<FoodItem> bankMemory, int index)
+        {
+            bankMemory.RemoveAt(index);
+            return bankMemory;
+        }
+
+        public static void PrintFoodItems(List<FoodItem> bankMemory)
+        {
+            if (bankMemory.Count == 0)
+            {
+                Console.WriteLine("\nThe food bank is empty.");
+            }
+            else
+            {
+                Console.WriteLine("\nFood Items:");
+                foreach (FoodItem foodItem in bankMemory)
+                {
+                    Console.WriteLine($"Name: {foodItem.Name}, Category: {foodItem.Category}, Quantity: {foodItem.Quantity}, Expiration Date: {foodItem.ExpirationDate}");
+                }
+            }
+        }
+
+        public static void ShowExpiredItems(List<FoodItem> bankMemory)
+        {
+            Console.WriteLine("\nExpired Items:");
+            foreach (FoodItem foodItem in bankMemory)
+            {
+                if (foodItem.ExpirationDate < DateTime.Now)
+                {
+                    Console.WriteLine($"Name: {foodItem.Name}, Category: {foodItem.Category}, Quantity: {foodItem.Quantity}, Expiration Date: {foodItem.ExpirationDate}");
+                }
+            }
+        }
+
+        public static void EditFoodItem(FoodItem foodItem)
+        {
+            //Display current attributes
+            Console.WriteLine($"Current attributes of {foodItem.Name}:");
+            Console.WriteLine($"1. Name: {foodItem.Name}");
+            Console.WriteLine($"2. Category: {foodItem.Category}");
+            Console.WriteLine($"3. Quantity: {foodItem.Quantity}");
+            Console.WriteLine($"4. Expiration Date: {foodItem.ExpirationDate}");
+
+            //Get user input
+            bool goCondition = true;
+            while (goCondition)
+            {
+                int index = 0;
+                try
+                {                  
+                    Console.WriteLine("\nWhich attribute would you like to edit?");
+                    index = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 4.");
+                }
+
+                switch (index)
+                {
+                    case 1:
+                        Console.Write("Enter new name: ");
+                        foodItem.Name = Console.ReadLine();
+                        goCondition = false;
+                        break;
+                    case 2:
+                        Console.Write("Enter new category: ");
+                        foodItem.Category = Console.ReadLine();
+                        goCondition = false;
+                        break;
+                    case 3:
+                        Console.Write("Enter new quantity: ");
+                        foodItem.Quantity = int.Parse(Console.ReadLine());
+                        goCondition = false;
+                        break;
+                    case 4:
+                        Console.Write("Enter new expiration date (YYYY-MM-DD): ");
+                        foodItem.ExpirationDate = DateTime.Parse(Console.ReadLine());
+                        goCondition = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nAttributes of {0} have been updated:", foodItem.Name);
+            Console.WriteLine($"1. Name: {foodItem.Name}");
+            Console.WriteLine($"2. Category: {foodItem.Category}");
+            Console.WriteLine($"3. Quantity: {foodItem.Quantity}");
+            Console.WriteLine($"4. Expiration Date: {foodItem.ExpirationDate}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,29 +3,131 @@
 //Initialize program
 Console.WriteLine("Program Loading...");
 
-//Initialize instances and variables
+//Initialize variables
 List<FoodItem> bankMemory = new List<FoodItem>();
-BankTools bankTools = new BankTools();
-
-//Print the menu
-Console.WriteLine("Welcome to the C# FoodBank!");
-Console.WriteLine("1. Add food item");
-Console.WriteLine("2. Delete food item");
-Console.WriteLine("3. Print food items");
-Console.WriteLine("4. Exit");
-Console.Write("Enter your choice (1-4): ");
+int userChoice = 0; 
 
 //Handle user input
-while (true)
+while (userChoice != 6) 
 {
-    try
+    //Print the menu
+    Console.Clear();
+    Console.WriteLine("\nWelcome to the C# FoodBank!");
+    Console.WriteLine("1. Add food item");
+    Console.WriteLine("2. Delete food item");
+    Console.WriteLine("3. Print food items");
+    Console.WriteLine("4. Show expired food items");
+    Console.WriteLine("5. Edit food items");
+    Console.WriteLine("6. Exit");
+    Console.Write("Enter your choice (1-6): ");
+
+    while (true)
     {
-        string choice = Console.ReadLine();
-        int userChoice = int.Parse(choice);
-        break;
+        try
+        {
+            string choice = Console.ReadLine();
+            userChoice = int.Parse(choice);
+            break;
+        }
+        catch
+        {
+            Console.WriteLine("Invalid input. Please enter a number between 1 and 5.");
+        }
     }
-    catch
+    switch (userChoice)
     {
-        Console.WriteLine("Invalid input. Please enter a number between 1 and 4.");
+        case 1: //Add food item
+            //Get user input
+            Console.WriteLine("Enter the name of the food item: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter the category of the food item: ");
+            string category = Console.ReadLine();
+
+            Console.WriteLine("Enter the quantity of the food item: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the expiration date of the food item (YYYY-MM-DD): ");
+            DateTime expirationDate = DateTime.Parse(Console.ReadLine());
+
+            //Add the food item to the bank
+            BankTools.AddFoodItem(bankMemory, name, category, quantity, expirationDate);
+
+            break;
+
+        case 2: //Delete food item
+            //Get user input about the index of the food item to delete
+            for (int i = 0; i < bankMemory.Count; i++) 
+            { 
+                Console.WriteLine($"{i + 1}. {bankMemory[i].Name}"); 
+            }
+                
+            Console.WriteLine("Enter the index of the food item to delete (or 0 to cancel): ");
+            int index = int.Parse(Console.ReadLine());
+            index -= 1;
+
+            if ((index < 0 && index != -1) || index >= bankMemory.Count)
+            {
+                Console.WriteLine("Invalid index. Please enter a number between 1 and " + bankMemory.Count + ".");
+                break;
+            }
+            else if (index >= 0 && index < bankMemory.Count)
+            {
+                bankMemory = BankTools.DeleteFoodItem(bankMemory, index);
+            }
+            else if (index == -1)
+            {
+                break;
+            }            
+
+            break;
+
+        case 3: //Print food items
+            BankTools.PrintFoodItems(bankMemory);
+
+            break;
+
+        case 4: //Show expired food items
+            BankTools.ShowExpiredItems(bankMemory);
+
+            break;
+
+        case 5: //Edit food items
+            //Get user input about the index of the food item to edit
+            for (int i = 0; i < bankMemory.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {bankMemory[i].Name}");
+            }
+
+            Console.WriteLine("Enter the index of the food item to edit (or 0 to cancel): ");
+            index = int.Parse(Console.ReadLine());
+            index -= 1;
+
+            if ((index < 0 && index != -1) || index >= bankMemory.Count)
+            {
+                Console.WriteLine("Invalid index. Please enter a number between 1 and " + bankMemory.Count + ".");
+                break;
+            }
+            else if (index >= 0 && index < bankMemory.Count)
+            {
+                BankTools.EditFoodItem(bankMemory[index]);
+            }
+            else if (index == -1)
+            {
+                break;
+            }
+
+            break;
+
+        case 6: //Exit
+            Console.WriteLine("\nGoodbye!");
+
+            break;
+
+        default: //Invalid choice
+            Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+            break;
     }
+    Console.WriteLine("\nPress Enter to continue...");
+    Console.ReadLine();
 }
